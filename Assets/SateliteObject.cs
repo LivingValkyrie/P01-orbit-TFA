3a39882034599b74e266a5fdf6bd644ebbdc9d58
LivingValkyrie <deadwynn@gmail.com>
2015-08-20 10:54:18 -0500

    manual merge
    imported nathans assets and made meta files visible

---
+++ Assets/SateliteObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: SateliteObject 
+/// </summary>
+public class SateliteObject : MonoBehaviour {
+    #region Fields
+
+    [Tooltip("The object that this object revolves around.")]
+    public Transform parentBody;
+
+    [Tooltip("The speed at which this object rotates")]
+    public float rotationSpeed;
+
+    [Tooltip("The speed at which this object revolves around its parentBody")]
+    public float revolutionSpeed;
+
+    #endregion
+
+    private void Start() {}
+
+    private void Update() {
+        
+        transform.RotateAround(parentBody.position,Vector3.up, (revolutionSpeed * Time.deltaTime * 1000) / 365);
+        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+
+    }
+
+}
---
+++ Assets/ScriptBorgShip.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptBorgShip : MonoBehaviour {
+
+    [Tooltip("Speed the borg ship rotates")]
+    public float rotationSpeed = 1.5f;
+
+    [Tooltip("Speed the borg ship moves")]
+    public float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, -(Time.deltaTime * rotationSpeed));
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.right, Time.deltaTime * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.right, -(Time.deltaTime * rotationSpeed));
+        }
+
+        transform.Translate(Vector3.forward * moveSpeed, Space.Self);
+	}
+}
